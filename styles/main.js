99b8260c71adc2472dcf90ef34a2ba8367895ae2
pkbsa <siranut.poko@hotmail.com>
2023-01-09 16:19:20 +0700

    add animation & fix font & certificate

---
+++ styles/main.js
@@ -0,0 +1,21 @@
+$(function () {
+  $("a[href*='#']").on("click", function (e) {
+    console.log("clicked");
+    e.preventDefault();
+    $("html, body").animate(
+      { scrollTop: $($(this).attr("href")).offset().top },
+      500,
+      "linear"
+    );
+  });
+  const toggleButton = document.getElementsByClassName("toggle-button")[0];
+  const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+  toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+  });
+  AOS.init({
+    duration: 3000,
+  })
+  
+  
+});
